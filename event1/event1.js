24dd8e3753235b0cc280a3793ca58c3d6900b43a
giorgibakhturidze <giorgi.bakhturidze28@gmail.com>
2022-01-18 01:07:22 +0400

    new17

---
+++ event1/event1.js
@@ -0,0 +1,6 @@
+let forSlide = document.querySelector(".for_slide");
+let stedy = document.querySelector('.slide-back');
+
+forSlide.addEventListener("click", function(){
+    stedy.classList.toggle('slide-back');
+})
